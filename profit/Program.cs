4a278ee377f641786ccb8b5618111c1485d36a4c
Stevie.wintec <steber05@student.wintec.ac.nz>
2019-03-11 09:22:59 +1300

    commit

---
+++ profit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[10];
+            Animal animal = null;
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animal = new Animal(123,22.4,"cow");
+                animals[i] = animal;
+            }
+            Animal nwe = new Animal();
+            return;
+        }
+    }
+}
